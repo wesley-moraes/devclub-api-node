f25e3c6afcf79780205bc1df4b0bd74f46a16e3b
wesley-moraes <wesleycrz@hotmail.com>
2024-08-11 16:24:56 -0300

    post

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 import express from 'express';
 
 const app = express();
-
+app.use(express.json());
 //Todas reprecisam de:
 //O tipo de rota/o método http(get, put, dete, etc...)
 //E em seguida precisa do endereço.
 //Resumidamente: 
 //1 - Tipo de Rota/Método HTTP
 //2 - Endereço.
+
+const users = [];
+
+app.post('/usuarios', (req, res) => {
+    //console.log(req.body);
+    users.push(req.body);
+    res.send('ok post');
+
+})
+
 app.get('/usuarios', (req, res) => {
-     res.send("ok, deu bom");
+     res.send(users);
 });
 
 app.listen(3000);
